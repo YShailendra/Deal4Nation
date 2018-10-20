99e48b31a3bfe8c7b00bfb172baeb5bc02a50670
shailendra.yadav1 <shailendra.yadav@brainplay.in>
2018-10-20 17:41:25 +0530

    code update for model update

--- Models/MarketPlaceModel.cs
+++ Models/MarketPlaceModel.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -12,6 +13,7 @@ namespace Products.Models
         [Required]
         public string Name { get; set;}
         public string Description { get; set;}
+        [NotMapped]
         public ImageModel Logo { get; set;}
         public string Url { get; set;}
         public Boolean? IsActive { get; set;}
--- Models/StoreModel.cs
+++ Models/StoreModel.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -11,6 +12,7 @@ namespace Products.Models
     {
         [Required]
         public string Name { get; set;}
+        [NotMapped]
         public  ImageModel Logo {get; set;}
     }
 }

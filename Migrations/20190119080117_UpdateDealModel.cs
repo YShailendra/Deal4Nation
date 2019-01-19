bb07bb340c7cc5ea6d6df966f957a9a3d0acc8dd
yshailendra <shailendra.yadav@codinova.com>
2019-01-19 13:32:53 +0530

    deal model update

---
+++ Migrations/20190119080117_UpdateDealModel.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Products.Migrations
+{
+    public partial class UpdateDealModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Image",
+                table: "Deals");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Image",
+                table: "Deals",
+                nullable: true);
+        }
+    }
+}
